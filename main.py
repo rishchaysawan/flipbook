d303ae3a3c28c0504aad60c6f496cb4924e92c9d
Nishchay Suteri <nishchay271095@gmail.com>
2015-10-06 16:03:33 +0530

    Add Profile Pic and Recommendation of Friends..

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ import hashlib
 import hmac
 import random
 import string
+import urllib2
+import json
+
 from google.appengine.ext import db
 from google.appengine.api import urlfetch
+from google.appengine.api import users
 SECRET="qwerty"
+URL="https://www.googleapis.com/oauth2/v3/tokeninfo?id_token="
 
 template_dir=os.path.join(os.path.dirname(__file__),'templates')
 jinja_env=jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),autoescape=True)
@@ -37,10 +42,23 @@ def valid_pw(name,pw,h):
     salt=h.split(',')[1]
     return h==make_pw_hash(name,pw,salt)
 
+def get_js(token):
+    url=URL+token
+    try:
+        content=urllib2.urlopen(url).read()
+    except URLError:
+        return
+    if content:
+        dictionary=json.loads(content)
+        return dictionary["name"],dictionary["email"]
+
 class Customer(db.Model):
-	email=db.StringProperty(required=True)
-	name=db.StringProperty(required=True)
-	password=db.StringProperty(required=True)
+    email=db.StringProperty(required=True)
+    name=db.StringProperty(required=True)
+    password=db.StringProperty(required=True)
+    profilepic=db.BlobProperty()
+    institution=db.StringProperty()
+    friend_count=db.IntegerProperty(default=0)
 
 class Products(db.Model):
     category=db.StringProperty(required=True)
@@ -161,25 +179,39 @@ class Show(Handler):
     def write_form(self):
         self.render("sellers.html")
     def get(self):
-        cursor=db.GqlQuery("Select * from Products")
+        cursor=db.GqlQuery("Select * from Customer where name='admin5' ")
+        #cursor=db.GqlQuery("Select * from Products where category='temp'")
         c=cursor.get()
-        self.response.headers['Content-Type'] = 'image/png'
-        self.write(c.image)
+        self.response.headers['Content-Type'] = 'image/jpg'
+        self.write(c.profilepic)
+        #self.write(c.image)
 
 class NewsHandler(Handler):
     def get(self):
+        error=""
         user_id=self.request.cookies.get('user_id')
         if user_id:
             result=check_secure_val(user_id)
             if result:
                 user=Customer.get_by_id(int(result))
                 self.write(user.email)
+                if len(user.institution):
+                    cursor=db.GqlQuery("Select * from Customer where institution ='%(institution)s'  AND name != '%(name)s' " %{'institution':user.institution,'name':user.name})
+                    c=cursor.get()
+
+                    if c:
+                        self.render("newsfeed.html",error=error,youmayknow=cursor)
+
+                    else:
+                        cursor=db.GqlQuery("Select * from Customer limit 10 ")    
+                        self.render("newsfeed.html",error=error,youmayknow=cursor)
+                else:
+                    error="please add institution"
+                    self.render("newsfeed.html",error=error) 
             else:
                 self.redirect("/")
         else:
                 self.redirect("/")
-
-
 
 class SignupHandler(Handler):
     def write_form(self,errorConfirm="",name="",password="",confirm="",mail="",userExists=""):
@@ -191,6 +223,8 @@ class SignupHandler(Handler):
         password=self.request.get('password')
         confirm_password=self.request.get('confirm_password')
         mail=self.request.get('email')
+        institution=self.request.get('institution')
+        profilepic=self.request.get('fileToUpload')
         errorMail=""
         errorUser=""
         errorConfirm=""
@@ -212,7 +246,7 @@ class SignupHandler(Handler):
                 self.write_form(errorConfirm,username,password,confirm_password,mail,"Please enter different email")
             else:
                 h_password=make_pw_hash(username,password)
-                obj=Customer(name=username,password=h_password,email=mail)
+                obj=Customer(name=username,password=h_password,email=mail,profilepic=profilepic,institution=institution)
                 obj.put()
                 user_id=obj.key().id()
                 user_id=str(user_id)
@@ -220,7 +254,30 @@ class SignupHandler(Handler):
                 self.response.headers.add_header('Set-Cookie','user_id=%s'%id_to_send)
                 self.redirect("/newsfeed")
 
+class TestHandler(Handler):
+    def get(self):
+        self.render("test.html")
+
+class ProductHandler(Handler):
+    def get(self):
+        product_id=self.request.get('pid')
+        cursor=Products.get_by_id(int(product_id))
+        #self.response.headers['Content-Type']='image/png'
+        #self.response.headers.add_header('Content-Type','image/png')
+        self.render("product.html",cursor=cursor)
+
+class BackendHandler(Handler):
+    def get(self):
+        token=self.request.get('token')
+        name,email=get_js(token)
+        self.write(name)
+        #self.render("backend.html",token=token)
+            #logging.warn(traceback.format_exc())
+        #token=self.request.get('token')
+        #self.render("backend.html",token=token)   
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),('/login',LoginHandler),('/loginEmail',LoginWithEmail),
-    ('/signup',SignupHandler),('/newsfeed',NewsHandler),('/sellers',Sellers),('/show',Show)
+    ('/signup',SignupHandler),('/newsfeed',NewsHandler),('/sellers',Sellers),('/show',Show),
+    ('/test',TestHandler),('/backend',BackendHandler),('/product',ProductHandler)
 ], debug=True)
